00a6c719c0a11f3f41fb4db863d12270e37ce45d
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-08-08 23:59:00 -0300

    Added function templates

--- marketplace-contract/contracts/WebWeaver.sol
+++ marketplace-contract/contracts/WebWeaver.sol
@@ -2,7 +2,65 @@
 
 pragma solidity ^0.8.9;
 
+import "./Ownable.sol";
 
-contract WebWeaver {
+struct Moderator {
+    address moderatorAddress;
+}
+
+struct Store {
+    uint storeID;
+    address owner;
+    uint stake;
+    //reputation?
+}
+
+enum Mode {
+    DIRECT,
+    AUTOMATIC_ESCROW,
+    MEDIATED
+}
+
+struct Product {
+    uint productID;
+    uint storeID;
+    uint price;
+    Mode[] modes;
+}
+
+contract WebWeaver is Ownable {
+
+    Moderator[] public moderators;
+    mapping(uint => Store) public stores;
+    mapping(uint => mapping(uint => Product)) public catalogs;
+
+
+    function registerStore() public onlyOwner {
+
+    }
+
+    function registerOrUpdateProduct() public {
+
+    }
+
+    function directPurchase() public payable {
+
+    }
+
+    function escrowPurchase() public payable {
+
+    }
+
+    function mediatedEscrowPurchase(address mediator) public payable {
+
+    }
+
+    function depositStoreStake() public {
+
+    }
+
+    function withdrawStoreStake() public {
+
+    }
 
 }
