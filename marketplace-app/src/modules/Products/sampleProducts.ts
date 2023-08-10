fd24d2a2274a9cc84c59302f3fae7310e2bc5513
Lucas Quinteiro <lucasq@bl3ndlabs.com>
2023-08-10 11:24:37 -0300

    feat: products page structure added

---
+++ marketplace-app/src/modules/Products/sampleProducts.ts
@@ -0,0 +1,52 @@
+const sampleProducts = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 100,
+    description: "This is product 1",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 200,
+    description: "This is product 2",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 300,
+    description: "This is product 3",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 400,
+    description: "This is product 4",
+  },
+  {
+    id: 5,
+    name: "Product 1",
+    price: 100,
+    description: "This is product 1",
+  },
+  {
+    id: 6,
+    name: "Product 2",
+    price: 200,
+    description: "This is product 2",
+  },
+  {
+    id: 7,
+    name: "Product 3",
+    price: 300,
+    description: "This is product 3",
+  },
+  {
+    id: 8,
+    name: "Product 4",
+    price: 400,
+    description: "This is product 4",
+  },
+];
+
+export default sampleProducts;
