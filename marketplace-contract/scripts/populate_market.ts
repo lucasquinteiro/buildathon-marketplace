55a6a5809beac4c33527dba1fd246bd0a0cb8c3c
matiasratcliffe <mati.ratcliffe@gmail.com>
2023-08-15 19:26:10 -0300

    ready for tests

--- marketplace-contract/scripts/populate_market.ts
+++ marketplace-contract/scripts/populate_market.ts
@@ -7,33 +7,13 @@ const yaml = require("js-yaml");
 
 import { getAccounts } from "./utils/accountsManager";
 import { Store, StoreData } from "./entities/store";
-import { deployMarket } from "./deploy_market";
 
 dotenv.config();
 
 
 const main = async () => {
-    if (process.argv.length > 5) {
-        console.error("You can only send one parameter [Contract address]");
-    }
-
-    var contractAddress: string = "";
-    if (process.argv.length == 5) {
-        contractAddress = process.argv[5];
-    } else {
-        contractAddress = process.env.CONTRACT_ADDRESS || "" ;
-    }
-
-    var contract: WebWeaver;
-    if (contractAddress == "") {
-        contract = await deployMarket();
-    } else {
-        contract = await ethers.getContractAt("WebWeaver", contractAddress);
-    }
-    
-
-    //aca me fijo si ya hay contrato deployado en el env.CONTRACT_ADDRESS, y sino, lo deployo
-    //llamo a populate aca
+    const contract: WebWeaver = await ethers.getContractAt("WebWeaver", process.env.CONTRACT_ADDRESS || "");
+    await populateMarket(contract);
 };
 
 const populateMarket = async (contract: WebWeaver) => {
@@ -55,13 +35,14 @@ const populateMarket = async (contract: WebWeaver) => {
     return stores;
 };
 
-main().then(() => {
-    console.log("Executed populate_market.ts script");
-    process.exitCode = 0;
-}).catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
-
+if (process.argv[1].endsWith("populate_market.ts")) {
+    main().then(() => {
+        console.log("Executed populate_market.ts script");
+        process.exitCode = 0;
+    }).catch((error) => {
+        console.error(error);
+        process.exitCode = 1;
+    });
+}
 
 export { populateMarket };
